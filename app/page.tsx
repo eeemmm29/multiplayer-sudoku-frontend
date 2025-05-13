6979dbf5dc8f085a9311b8bf6796f7f054f5676f
Emir <148708322+eeemmm29@users.noreply.github.com>
2025-05-13 13:58:00 +0600

    Clean up comments and imports, show text on main page in separate lines

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,8 @@ export default function Home() {
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
         <span className={title()}>マルチプレイヤー数独</span>
-        <span className={title()}>Multiplayer Sudoku</span>
-        {/* <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
         <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div> */}
+        <span className={title()}>Multiplayer Sudoku</span>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 min-h-48">
@@ -38,19 +31,6 @@ export default function Home() {
           labelEnglish="Create a Room"
         />
       </div>
-
-      {/* <div className="flex gap-3"> */}
-      {/* <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link> */}
       <Link
         isExternal
         className={buttonStyles({ variant: "bordered", radius: "full" })}
@@ -59,15 +39,6 @@ export default function Home() {
         <GithubIcon size={20} />
         GitHub
       </Link>
-      {/* </div> */}
-
-      {/* <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div> */}
     </section>
   );
 }
